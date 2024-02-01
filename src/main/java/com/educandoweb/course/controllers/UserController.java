27c3740c3bb94b0953bbf77c9b54cfdf7bc35ec7
Augusto Nunes <augustocesar.udi@gmail.com>
2024-02-01 10:21:28 -0300

    User delete

--- src/main/java/com/educandoweb/course/controllers/UserController.java
+++ src/main/java/com/educandoweb/course/controllers/UserController.java
@@ -36,4 +36,10 @@ public class UserController {
                 .path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
